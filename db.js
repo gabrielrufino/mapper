3b7596cfa94095b453eaf9bee3dccf61ea1b9120
Gabriel Rufino <>
2019-01-17 21:33:41 -0300

    Loading database with environment variables

--- db.js
+++ db.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true})
+
+const host     = process.env.DB_HOST
+const port     = process.env.DB_PORT
+const database = process.env.DB_DATABASE
+const username = process.env.DB_USERNAME
+const password = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb://${username}:${password}@${host}:${port}/${database}`, {
+  useNewUrlParser: true
+})
 
 module.exports = {}
