56dfcbdd09dedae0766466f9d820cbe6769ca105
Gabriel Rufino <>
2019-01-17 22:16:20 -0300

    Default values to loading the database

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-const host     = process.env.DB_HOST
-const port     = process.env.DB_PORT
-const database = process.env.DB_DATABASE
+const host     = process.env.DB_HOST     || 'localhost'
+const port     = process.env.DB_PORT     || '27017'
+const database = process.env.DB_DATABASE || 'db_mapper'
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
 
