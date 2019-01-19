86a631c98b993b1e57814cf8cd53642f682e5d63
Gabriel Rufino <>
2019-01-18 22:20:13 -0300

    moving app.listen to app.js

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const app     = express()
 app.use('/users', require('./routes/users'))
 app.use('/', require('./controllers/index'))
 
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log('mapper-api listening on', port))
+
 module.exports = app
--- index.js
+++ index.js
@@ -4,7 +4,3 @@ dotenv.config()
 const app = require('./app')
 const db  = require('./db')
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
-  console.log('mapper listening on', port)
-})
