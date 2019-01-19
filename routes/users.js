b2807971f36bbee456809446401dd68d69d19b20
Gabriel Rufino <>
2019-01-19 10:31:42 -0300

    importing users controllers in the users router

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,17 @@
 const express = require('express')
+
+const create  = require('../controllers/users/create')
+const readAll = require('../controllers/users/readAll')
+const readOne = require('../controllers/users/readOne')
+const update  = require('../controllers/users/update')
+const del     = require('../controllers/users/delete')
+
 const router  = express.Router()
 
-router.post('/', (req, res) => {
-  res.send('POST /users')
-})
-
-router.get('/', (req, res) => {
-  res.send('GET /users')
-})
-
-router.get('/:id', (req, res) => {
-  res.send('GET /users/:id')
-})
-
-router.put('/:id', (req, res) => {
-  res.send('PUT /users/:id')
-})
-
-router.delete('/:id', (req, res) => {
-  res.send('DELETE /users/:id')
-})
+router.post('/', create)
+router.get('/', readAll)
+router.get('/:id', readOne)
+router.put('/:id', update)
+router.delete('/:id', del)
 
 module.exports = router
