01bf1a2305b18e84c8d893e57c48222e904b52d1
Gabriel Rufino <>
2019-01-19 14:07:03 -0300

    Controller to list all the users

--- controllers/users/readAll.js
+++ controllers/users/readAll.js
@@ -1,5 +1,10 @@
-const readAll = (req, res) => {
-  res.send('GET /users')
+const mongoose = require('mongoose')
+const User     = mongoose.model('User')
+
+const readAll = async (req, res) => {
+  const users = await User.find({})
+
+  return res.json(users)
 }
 
 module.exports = readAll
