565e2e4dc8725cd98d122db02697cc5cd223bd48
Gabriel Rufino <>
2019-01-19 14:19:30 -0300

    update controller to user

--- controllers/users/update.js
+++ controllers/users/update.js
@@ -1,5 +1,17 @@
-const update = (req, res) => {
-  res.send('PUT /users/:id')
+const mongoose = require('mongoose')
+const User     = mongoose.model('User')
+
+const update = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id })
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' })
+  }
+
+  user.name     = req.body.name
+  user.username = req.body.username
+  user.save()
+
+  return res.json(user)
 }
 
 module.exports = update
