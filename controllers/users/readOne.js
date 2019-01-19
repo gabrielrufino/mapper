bd29d3940f57a6b11eb2a4b565c74716c5e38542
Gabriel Rufino <>
2019-01-19 14:11:58 -0300

    Controller to return one user

--- controllers/users/readOne.js
+++ controllers/users/readOne.js
@@ -1,5 +1,13 @@
-const readOne = (req, res) => {
-  res.send('GET /users/:id')
+const mongoose = require('mongoose')
+const User     = mongoose.model('User')
+
+const readOne = async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id })
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' })
+  }
+
+  return res.json(user)
 }
 
 module.exports = readOne
