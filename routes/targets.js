9e2244ca080f9f77e4f7d2231d76857f5234fea9
Gabriel Rufino <>
2019-01-18 22:30:09 -0300

    routes ok!

--- routes/targets.js
+++ routes/targets.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const router  = express.Router()
 
+router.post('/', (req, res) => {
+  res.send('POST /targets')
+})
 
+router.get('/', (req, res) => {
+  res.send('GET /targets')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /targets/:id')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /targets/:id')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /targets/:id')
+})
